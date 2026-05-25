42e16c5e251e65b7320b77ccf2f3817f29c263e6
SoyElquin <soyelquin@hotmail.com>
2026-05-25 11:37:41 -0500

    dd

--- src/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="loading-screen" aria-label="Cargando portafolio">
-      <div className="loading-orb" />
-      <span>Cargando portafolio</span>
-    </div>
-  );
-}
